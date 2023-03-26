3b8090ec4192a3cad922ae458de600135e6b653a
imsonishivam <imsonishivam@gmail.com>
2023-03-26 20:02:47 +0530

    Home & about

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
